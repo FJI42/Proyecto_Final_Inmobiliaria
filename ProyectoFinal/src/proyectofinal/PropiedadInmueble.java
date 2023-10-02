ee7cd3a8118067c432eee0ca9f48ef5db7df64a9
Milena <32686092+xIX-Mile-XIx@users.noreply.github.com>
2023-10-02 11:57:50 -0300

    propietario y  propiedad inmueble

---
+++ ProyectoFinal/src/proyectofinal/PropiedadInmueble.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author MCali
+ */
+public class PropiedadInmueble {
+    private String accesibilidad ; 
+    private String caracteristicas ; 
+    private String dirección ;
+    private Propietario dueño; 
+    private String estadoLocal;
+    private String forma; 
+    private int idLocal; 
+    private Inquilino ocupante; 
+    private float precioTazado;
+    private Inspector revisor;  
+    private int superficieMinima;
+    private String tipoLocal;
+    private String zona;
+
+}
---
+++ ProyectoFinal/src/proyectofinal/Propietario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author MCali
+ */
+public class Propietario {
+    private String apelidoPropietario; 
+    private int dni;
+    private String domicilio;
+    private int idPropietario;
+    private String localesPropios;
+    private String nombrePropietario;
+    private int telefono;
+
+}
